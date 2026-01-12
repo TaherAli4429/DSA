451007af955df25d053827d32fcc6b7b18bf2709
TaherAli4429 <tharadrataher@gmail.com>
2026-01-12 11:21:24 -0500

    Time: 0 ms (100%), Space: 9.9 MB (21.9%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int minTimeToVisitAllPoints(int** points, int pointsSize, int* pointsColSize) {
+    int totalTime = 0;
+
+    for (int i = 0; i < pointsSize - 1; i++) {
+        int x1 = points[i][0];
+        int y1 = points[i][1];
+        int x2 = points[i + 1][0];
+        int y2 = points[i + 1][1];
+
+        int dx = abs(x2 - x1);
+        int dy = abs(y2 - y1);
+
+        // Time needed to move between two points
+        totalTime += (dx > dy) ? dx : dy;
+    }
+
+    return totalTime;
+}
